3cf583adf5f75b08d02f994a9f51a73eb15be736
zakariabijoy <zakaria.bijoy@live.com>
2021-12-14 03:06:39 +0600

    typescript base type & primitives

---
+++ typescript intro/basics.ts
@@ -0,0 +1,16 @@
+//data types-------------------------------------------------------------------
+//primitive types: number, string, boolean
+//more complex types: array, object
+//function types, parameters
+
+//primitives----
+
+let age:number = 25;
+
+age = 12;
+
+let userName: string="bijoy";
+
+let isTrue:boolean = true;
+
+//null & undefined  also premitive type

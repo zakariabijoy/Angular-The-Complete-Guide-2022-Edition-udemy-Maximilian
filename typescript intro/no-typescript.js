185f433cf459a7e53fa0ff3a454fb7146bbfbec9
zakariabijoy <zakaria.bijoy@live.com>
2021-12-14 03:05:54 +0600

    installing & using typescript

---
+++ typescript intro/no-typescript.js
@@ -0,0 +1,6 @@
+function add(a,n){
+    return a+blur;
+}
+
+const result = add(5,2);
+console.log(result);
---
+++ typescript intro/with-typescript.ts
@@ -0,0 +1,6 @@
+function add1 (a:number,b:number){
+    return a+b;
+}
+
+const result1 = add1(1,2);
+console.log(result1);

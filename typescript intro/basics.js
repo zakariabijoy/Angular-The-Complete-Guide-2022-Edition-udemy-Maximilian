5a5f2e2470659dc76cc975db1291a6dc379982ce
Zakaria Masud <zakaria.masud@surbanajurong.com>
2022-10-03 01:11:24 +0600

    array and object types -typescript intro

--- typescript intro/basics.js
+++ typescript intro/basics.js
@@ -2,38 +2,20 @@
 //primitive types: number, string, boolean
 //more complex types: array, object
 //function types, parameters
-
 //primitives---------------
-
-let age:number = 25;
-
+var age = 25;
 age = 12;
-
-let userName: string="bijoy";
-
-let isTrue:boolean = true;
-
+var userName = "bijoy";
+var isTrue = true;
 // Note: null & undefined  also premitive type
-
-
 // more complex type--------------
-
 // array----
-let hobbies: string[];
-hobbies = ['Sports','Çooking'];
-
+var hobbies;
+hobbies = ['Sports', 'Çooking'];
 // object---
-let person: {
-    name:string;
-    age: number;
-};
-
+var person;
 person = {
-    name:'Max',
+    name: 'Max',
     age: 32
 };
-
-let people: {
-    name:string;
-    age: number;
-}[];
+var people;
---
+++ typescript intro/with-typescript.js
@@ -0,0 +1,5 @@
+function add1(a, b) {
+    return a + b;
+}
+var result1 = add1(1, 2);
+console.log(result1);

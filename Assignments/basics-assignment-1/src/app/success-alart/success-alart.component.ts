aafceb7f761a4d096706feae13b81d4e1bf5cca8
zakariabijoy <zakaria.bijoy@live.com>
2022-10-16 02:43:33 +0600

    done basic-assignment-1

---
+++ Assignments/basics-assignment-1/src/app/success-alart/success-alart.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-success-alart",
+  template: ` <div class="alert alert-success" role="alert">
+    A simple success alert—check it out!
+  </div>`,
+  styleUrls: ["./success-alart.component.css"],
+})
+export class SuccessAlartComponent {}
---
+++ Assignments/basics-assignment-1/src/app/warning-alart/warning-alart.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alart',
+  templateUrl: './warning-alart.component.html',
+  styleUrls: ['./warning-alart.component.css']
+})
+export class WarningAlartComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

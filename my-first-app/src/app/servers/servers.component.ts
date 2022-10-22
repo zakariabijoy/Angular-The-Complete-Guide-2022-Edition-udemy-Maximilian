fae9e1004238adb97d19577c2cd971e8c3e4d88b
zakariabijoy <zakaria.bijoy@live.com>
2022-10-23 02:30:11 +0600

    combining all forms of data binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -24,7 +24,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created';
+    this.serverCreationStatus = 'Server was created! Name is' + this.serverName;
   }
 
   onUpdateServerName(event: Event) {

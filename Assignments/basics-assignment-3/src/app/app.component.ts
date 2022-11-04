5ea573631517d871978e9c29f8a00845333ed30f
zakariabijoy <zakaria.bijoy@live.com>
2022-11-05 02:04:46 +0600

    The Basics: assignment 3 is done

---
+++ Assignments/basics-assignment-3/src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  showSecret = false;
+  logs = [];
+
+  onToggleDetails() {
+    this.showSecret = !this.showSecret;
+    this.logs.push(this.logs.length + 1);
+  }
+}
